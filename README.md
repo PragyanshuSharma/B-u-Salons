b35a1f0e9dc1574e99a60c4ba3ca9fda308773f5
PragyanshuSharma <pragyanshu.sharma@daffodilsw.com>
2020-08-16 10:54:22 +0530

    4 APIs as per problem statement

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# test_B-u-Salons
-Test repo for B U Salon
+# test-B-Salons
+test-B-Salons Test API 
+Node version v12.13.1
+Mongodb version 4.2.2
+
+
+All APIs except login and signUp are accessible with token only.[i.e Require auth]
+[POST]http://localhost:3001/bSalons/api/v1/public/user/signUpWithEmail     User SignUp
+[POST]http://localhost:3001/bSalons/api/v1/public/user/login               Login
+[POST]http://localhost:3001/bSalons/api/v1/user/order                      Add order
+[GET]http://localhost:3001/bSalons/api/v1/user/order                Get accumulated user and order data with pagination
+
+Steps to use APIs
+1.git clone repository
+2.Install pm2 globally  npm i -g pm2
+2.pm2 start  server.js -i max or node server.js to start without pm2
+ 
+ APIs started at port 3001 by default
---
+++ modules/User/routes/login.js
@@ -0,0 +1,26 @@
+const route = require('../../../lib/route'),
+  validate = require('../../../lib/validate'),
+  logger = require('../../../lib/logger'),
+  UserController = require('../UserController'),
+  apiResponse = require('../../../lib/apiResponse');
+
+const validateBody = {
+  type: 'object',
+  properties: {
+    email: { type: 'string', format: 'email' },
+    password: { type: 'string', format: 'password', minlength: 8 }
+  },
+  required: ['email', 'password'],
+  additionalProperties: false
+};
+
+const loginHandler = async (req, res) => {
+  validate(req.body, validateBody);
+
+
+  const result = await UserController.loginHandler(req.body);
+  res.setHeader('authorization', result.token);
+  return apiResponse(200, result, 'Login sucess', true, res);
+};
+
+module.exports = route.post('/user/login', loginHandler, { isPublic: true });
